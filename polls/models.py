87d2e05866b657299ee26bfd213b6b3601612892
Ferez <minh9a4@gmail.com>
2021-09-06 23:12:06 +0700

    good place to hold back and start CI/CD stuffs

---
+++ polls/models.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255, null=False)
+    pub_date = models.DateTimeField('date published')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def __str__(self):
+        return "%s. %s" % (self.id, self.question_text)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255, null=False)
+    votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s. %s" % (self.question.id, self.choice_text)
